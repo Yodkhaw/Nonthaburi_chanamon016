6db48abc90750bab616b51ccfb41cea0d58ea479
Yodkhaw <124548266+Yodkhaw@users.noreply.github.com>
2023-04-24 19:12:34 +0700

    Add files via upload

---
+++ nonthaburi.js
@@ -0,0 +1,155 @@
+ {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "เกาะเกร็ด",
+          "type": "ตลาด",
+          "level": 7
+        },
+        "geometry": {
+          "coordinates": [
+            100.4770178617569,
+            13.90977553317991
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "เซ็ลทรัล เวสเกต",
+          "type": "ห้างสรรพสินค้า",
+          "level": 8
+        },
+        "geometry": {
+          "coordinates": [
+            100.41164829661346,
+            13.876965052541962
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "อิเกีย บางใหญ่",
+          "type": "ห้างสรรพสินค้า",
+          "level": 8
+        },
+        "geometry": {
+          "coordinates": [
+            100.41251847378294,
+            13.878750287397807
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "บีบี มาร์เก็ต",
+          "type": "ตลาด",
+          "level": 6
+        },
+        "geometry": {
+          "coordinates": [
+            100.40846786894417,
+            13.877003461251832
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "วัดไทรใหญ่",
+          "type": "วัด",
+          "level": 5
+        },
+        "geometry": {
+          "coordinates": [
+            100.31081045246623,
+            13.978568479088466
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "วัดเล่งเน่ยยี่2",
+          "type": "วัด",
+          "level": 4
+        },
+        "geometry": {
+          "coordinates": [
+            100.42142180945797,
+            13.913089819065306
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "วัดสนามเหนือ",
+          "type": "วัด",
+          "level": 3
+        },
+        "geometry": {
+          "coordinates": [
+            100.49246954397734,
+            13.912855615346842
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ตลาดเลี่ยงเมืองนนท์",
+          "type": "ตลาด",
+          "level": 5
+        },
+        "geometry": {
+          "coordinates": [
+            100.49501191534665,
+            13.868949588570587
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ท่าน้ำนนท์",
+          "type": "ท่าน้ำ",
+          "level": 4
+        },
+        "geometry": {
+          "coordinates": [
+            100.49188454232655,
+            13.842380585726417
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "สวนเฉลิมพระเกียรติ",
+          "type": "สวน",
+          "level": 4
+        },
+        "geometry": {
+          "coordinates": [
+            100.5307559222291,
+            13.880958813078067
+          ],
+          "type": "Point"
+        }
+      }
+    ]
+  }
